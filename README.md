5f1bcd4c370cac996d2de48d1511176397d019dd
getbehindthmule <gerardsavage@me.com>
2020-02-10 12:04:27 +0000

    include KMS. table name encrypted as environmwnr variable and decrypted at runtime

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aws-lambda
+
+Includes using KMS to obtain the db table name to be used. Dt encrypted locally vis the cli. Remember that th ciphertext is base64-encoded and needs to be decoded before decrypted
--- src/main/java/greenhills/GetCompanyHandler.java
+++ src/main/java/greenhills/GetCompanyHandler.java
@@ -5,11 +5,18 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.DecryptRequest;
+import com.amazonaws.services.kms.model.DecryptResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import greenhills.dto.Company;
 import greenhills.dto.GetCompanyRequest;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,16 +26,27 @@ public class GetCompanyHandler implements RequestHandler<GetCompanyRequest, Comp
     private static final String NAME = "name";
     private static final AmazonDynamoDB AMAZON_DYNAMO_DB = AmazonDynamoDBClientBuilder.defaultClient();
     private static final String ERROR_UNKNOWN_COMPANY = "ERROR: UNKNOWN COMPANY";
-
+    private static final String ENCRYPTED_TABLE_NAME = "ENCRYPTED_TABLE_NAME";
     @Override
     public Company handleRequest(GetCompanyRequest input, Context context) {
         HashMap<String, AttributeValue> key = new HashMap<String,AttributeValue>(){{put(ID, new AttributeValue().withN(input.getId().toString()));}};
-        String companyName ;
-        context.getLogger().log("table name is " + System.getenv("TABLE_NAME"));
+        String companyName, decryptedTableName ;
+
+        byte[] decodedBytes = Base64.getDecoder().decode(System.getenv(ENCRYPTED_TABLE_NAME));
+        AWSKMS client = AWSKMSClientBuilder.defaultClient();
+        DecryptRequest decryptRequest = new DecryptRequest()
+                .withCiphertextBlob(ByteBuffer.wrap(decodedBytes));
+        DecryptResult response = client.decrypt(decryptRequest);
+        try {
+            decryptedTableName = new String(response.getPlaintext().array(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            context.getLogger().log("failed convert" + e.getMessage());
+            decryptedTableName = null;
+        }
 
         GetItemRequest getItemRequest = new GetItemRequest()
                 .withKey(key)
-                .withTableName(System.getenv("TABLE_NAME"));
+                .withTableName(decryptedTableName);
 
         try {
             Map<String,AttributeValue> returnedItem = AMAZON_DYNAMO_DB.getItem(getItemRequest).getItem();
